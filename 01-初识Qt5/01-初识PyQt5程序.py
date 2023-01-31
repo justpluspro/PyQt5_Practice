95f5deb0682b0973a863af8a6e556e4c86df5dba
qwli7 <qwli7@iflytek.com>
2023-01-31 11:19:23 +0800

    PyQt5 hello world

---
+++ 01-初识Qt5/01-初识PyQt5程序.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.show()
+window.setWindowTitle("Python")
+
+# app.setStyleSheet()
+
+
+sys.exit(app.exec_())
