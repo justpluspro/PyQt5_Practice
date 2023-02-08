8c0be10815da12110fdc2a0a1f3c843480b0f405
qwli7 <228794754@qq.com>
2023-02-08 19:15:37 +0800

    PyQt5 QTableWidget

--- 05-基本控件/11-QTableWidget.py
+++ 05-基本控件/11-QTableWidget.py
@@ -7,7 +7,7 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QWidget, QApplication, QTableWidget, QHBoxLayout, QVBoxLayout, QPushButton, QFrame, \
-    QTableWidgetItem
+    QTableWidgetItem, QHeaderView, QAbstractItemView
 
 
 class MainWindow(QWidget):
@@ -35,7 +35,17 @@ class MainWindow(QWidget):
         self.table.horizontalHeader().setSectionsClickable(False) #表头禁止点击
         self.table.horizontalHeader().setStyleSheet('QHeaderView::section{background:green}') #　设置表头为绿色
 
+        # 让表头铺满整个 table 控件
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
+        # 双击单元格，禁止编辑
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        # 禁止编辑单元格后，双击鼠标，选中的是该单元格，如果要双击选中整行，可以按照如下操作
+        # 另外还可以将该选项调整成选中一列，选中单元格
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        # 隐藏表头
+        self.table.horizontalHeader().setVisible(False)
 
 
         main_layout = QHBoxLayout()
@@ -46,31 +56,46 @@ class MainWindow(QWidget):
         add_row_btn.clicked.connect(self.add_row)
         delete_row_btn = QPushButton('减少一行')
         delete_row_btn.clicked.connect(self.delete_row)
-        editable_row_btn = QPushButton('可以编辑')
+        self.editable_row_btn = QPushButton('禁止/启用编辑')
+        self.editable_row_btn.setCheckable(True)
+        self.editable_row_btn.clicked.connect(self.disable_edit)
         choose_row_btn = QPushButton('选择整行')
 
         button_group_layout.addWidget(add_row_btn)
         button_group_layout.addWidget(delete_row_btn)
-        button_group_layout.addWidget(editable_row_btn)
+        button_group_layout.addWidget(self.editable_row_btn)
         button_group_layout.addWidget(choose_row_btn)
         main_layout.addWidget(self.table)
         main_layout.addLayout(button_group_layout)
 
         self.setLayout(main_layout)
 
+
     def add_row(self):
         self.row_count = self.table.rowCount() # 获取当前总行，用作删除一行使用
         print(self.row_count)
+        self.table.insertRow(self.row_count)
         self.table.setItem(self.row_count, 0, QTableWidgetItem("1"))
         self.table.setItem(self.row_count, 1, QTableWidgetItem('张三'))
-        self.table.setItem(self.row_count, 2, QTableWidgetItem(25))
-        self.table.setItem(self.row_count, 3, QTableWidgetItem(11111))
+        self.table.setItem(self.row_count, 2, QTableWidgetItem(str(25)))
+        self.table.setItem(self.row_count, 3, QTableWidgetItem(str(11111)))
         self.table.setItem(self.row_count, 4, QTableWidgetItem("湖北荆州"))
         self.row_count += 1
         self.table.setRowCount(self.row_count)
 
     def delete_row(self):
         print(self.row_count)
+        if self.row_count == 0:
+            return
+        self.row_count -= 1
+        self.table.setRowCount(self.row_count)
+
+    def disable_edit(self):
+        if not self.editable_row_btn.isChecked():
+            # 双击单元格，禁止编辑
+            self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        else:
+            self.table.setEditTriggers()
 
 def main():
     app = QApplication(sys.argv)
